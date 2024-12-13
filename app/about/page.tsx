11999b0f4bf2b8acbe6d1b9097a590b3b00d4e81
chansamangrathana <chansamangrathana1607@gmail.com>
2024-12-13 11:19:40 +0700

    about us 90%

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,9 +13,9 @@ import { motion } from "framer-motion";
 
 export default function about() {
   return (
-    <main className="">
+    <div>
       {/* Hero Section */}
-      <section className="container mx-auto px-6 py-12 md:py-24">
+      <section className="container mx-auto px-6 ">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           {/* Text Section */}
           <motion.div
@@ -44,7 +44,7 @@ export default function about() {
             transition={{ duration: 0.8, ease: "easeOut" }}
           >
             <Image
-              src="/about-us/1.png"
+              src="/about-us/1.1.png"
               alt="Programming Education Illustration"
               fill
               className="object-contain rounded-lg"
@@ -54,13 +54,13 @@ export default function about() {
       </section>
 
       {/* Features Section */}
-      <div className=" bg-gray-100">
-        <section className=" mx-16 py-16">
-          <div className="container mx-auto px-10 ">
+      <div className="">
+        <section className="w-full py-16">
+          <div className="container mx-auto px-6">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
               {/* Goal Feature */}
-              <div className="text-center space-y-4 ">
-                <div className="flex justify-center ">
+              <div className="text-center space-y-4">
+                <div className="flex justify-center">
                   <Image
                     src="/about-us/vision.png" // Replace with your logo path
                     alt="Code Advisors Logo"
@@ -68,8 +68,8 @@ export default function about() {
                     height={100}
                   />
                 </div>
-                <h3 className="text-[24px] font-semibold  text-primary">
-                  បេសកកម្ម
+                <h3 className="text-[24px] font-semibold text-primary">
+                  បេសកម្ម
                 </h3>
                 <p className="text-gray-600 flex justify-center text-[18px]">
                   ផ្តល់វិធីសាស្រ្តក្នុងការចែករំលែក ចំណេះដឹង ការដោះស្រាយបញ្ហា
@@ -78,8 +78,8 @@ export default function about() {
               </div>
 
               {/* Meaning Feature */}
-              <div className="text-center space-y-4 ">
-                <div className="flex justify-center ">
+              <div className="text-center space-y-4">
+                <div className="flex justify-center">
                   <Image
                     src="/about-us/vision.png" // Replace with your logo path
                     alt="Code Advisors Logo"
@@ -87,7 +87,7 @@ export default function about() {
                     height={100}
                   />
                 </div>
-                <h3 className="text-[24px] font-semibold  text-primary">
+                <h3 className="text-[24px] font-semibold text-primary">
                   ចក្ខុវិស័យ
                 </h3>
                 <p className="text-gray-600 flex justify-center text-[18px]">
@@ -97,8 +97,8 @@ export default function about() {
               </div>
 
               {/* Quality Feature */}
-              <div className="text-center space-y-4 ">
-                <div className="flex justify-center ">
+              <div className="text-center space-y-4">
+                <div className="flex justify-center">
                   <Image
                     src="/about-us/vision.png" // Replace with your logo path
                     alt="Code Advisors Logo"
@@ -106,7 +106,7 @@ export default function about() {
                     height={100}
                   />
                 </div>
-                <h3 className="text-[24px] font-semibold  text-primary">
+                <h3 className="text-[24px] font-semibold text-primary">
                   គុណតម្លៃ
                 </h3>
                 <p className="text-gray-600 flex justify-center text-[18px]">
@@ -118,8 +118,9 @@ export default function about() {
           </div>
         </section>
       </div>
+
       {/* About Section */}
-      <section className="container mx-auto px-4 py-16">
+      <section className="container mx-auto px-6 py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="relative h-[300px] md:h-[350px] lg:h-[400px]">
             <Image
@@ -141,8 +142,8 @@ export default function about() {
         </div>
       </section>
       {/* Learning Path Section */}
-      <section className=" text-white py-16">
-        <div className="container mx-auto px-4">
+      <section className=" text-white  py-16">
+        <div className="container mx-auto px-6">
           <h2 className="text-center text-3xl font-bold text-secondary mb-12">
             ការផ្តល់ពិន្ទុទៅតាមកម្រិត
           </h2>
@@ -218,7 +219,7 @@ export default function about() {
             />
           </div>
           <div className="space-y-6">
-            <h2 className="text-4xl font-extrabold text-primary">ទំនាក់ទំនង</h2>
+            <h2 className="text-4xl font-extrabold text-secondary">ទំនាក់ទំនង</h2>
             <p className="text-lg text-gray-700 leading-relaxed">
               CodeAdvisors ទទួលនូវរាល់មតិទាំងឡាយពីអ្នកប្រើប្រាស់
               ជាទីស្រលាញ់​របស់ពួកយើង
@@ -239,6 +240,6 @@ export default function about() {
           </div>
         </div>
       </section>
-    </main>
+    </div>
   );
 }
--- components/navbar/NavbarLogin.tsx
+++ components/navbar/NavbarLogin.tsx
@@ -1,70 +1,69 @@
+import { Dropdown, DropdownItem, Navbar, NavbarBrand } from "flowbite-react";
 import {
- 
-    Dropdown,
-    DropdownItem,
-    Navbar,
-    NavbarBrand,
-  } from "flowbite-react";
-  import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-  import { LogOut, Moon, Settings, User } from 'lucide-react'
-  import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-  import { Switch } from "@/components/ui/switch"
-  import Image from "next/image";
-  import { FiSearch, FiBell, FiEdit2 } from "react-icons/fi";
-  
-  export function NavbarLogin() {
-    return (
-      <Navbar fluid rounded className="flex z-[100] items-center px-4 justify-between h-[72px] mx-[80px]">
-        {/* Navbar Brand */}
-        <NavbarBrand href="/">
-          <div className="mr-16">
-            <Image
-              src="/logo1.png" // Replace with your logo path
-              alt="Code Advisors Logo"
-              width={100}
-              height={100}
-            />
-          </div>
-        </NavbarBrand>
-  
-        {/* Search Bar */}
-        <div className="flex flex-1 justify-center">
-                <div className="relative w-[800px]">
-                    <input
-                        type="text"
-                        placeholder="ស្វែងរក"
-                        className="w-full h-[35px] text-sm rounded-[5px] border border-gray-300 pl-4 pr-10 focus:outline-none "
-                    />
-                    <button className="absolute right-2 top-1/2 -translate-y-1/2 p-[5px] ">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="w-5 h-5"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M21 21l-4.35-4.35m2.85-6.15a7 7 0 11-14 0 7 7 0 0114 0z"
-                            />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-  
-        {/* Action Icons */}
-        <div className="flex items-center mx-8  ">
-         
-         <div className=" bg-primary px-4  rounded-md text-white"> {/* Button with Dropdown */}
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut, Moon, Settings, User } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Switch } from "@/components/ui/switch";
+import Image from "next/image";
+import { FiSearch, FiBell, FiEdit2 } from "react-icons/fi";
+
+export function NavbarLogin() {
+  return (
+    <Navbar
+      fluid
+      rounded
+      className="flex z-[100] items-center px-4 justify-between h-[72px] mx-[80px]"
+    >
+      {/* Navbar Brand */}
+      <NavbarBrand href="/">
+        <div className="mr-16">
+          <Image
+            src="/logo1.png" // Replace with your logo path
+            alt="Code Advisors Logo"
+            width={100}
+            height={100}
+          />
+        </div>
+      </NavbarBrand>
+
+      {/* Search Bar */}
+      <div className="flex flex-1 justify-center">
+        <div className="relative w-[800px]">
+          <input
+            type="text"
+            placeholder="ស្វែងរក"
+            className="w-full h-[35px] text-sm rounded-[5px] border border-gray-300 pl-4 pr-10 focus:outline-none "
+          />
+          <button className="absolute right-2 top-1/2 -translate-y-1/2 p-[5px] ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-4.35-4.35m2.85-6.15a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      {/* Action Icons */}
+      <div className="flex items-center mx-8  ">
+        <div className=" bg-primary px-4  rounded-md text-white">
+          {" "}
+          {/* Button with Dropdown */}
           <Dropdown
             inline
             label={
@@ -77,62 +76,59 @@ import {
             <DropdownItem className="text-black">បង្កើតអត្ថបទ</DropdownItem>
             <DropdownItem className="text-black">បង្កើត Forum</DropdownItem>
           </Dropdown>
-          </div>
-  
-          {/* Notification Icon */}
-          <button className="relative text-primary mx-8 ">
-            <FiBell className="h-7 w-7" />
-            <span className="absolute -top-1 -right-1 flex h-3 w-3 items-center justify-center rounded-full bg-red-500 text-xs text-white">
-              
-            </span>
-          </button>
-  
-          {/* User Avatar */}
-          <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Avatar className="h-8 w-8 cursor-pointer">
-            <AvatarImage src="/user.jpg" alt="User avatar" />
-            <AvatarFallback></AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end">
-          {/* <DropdownMenuLabel>
+        </div>
+
+        {/* Notification Icon */}
+        <button className="relative text-primary mx-8 ">
+          <FiBell className="h-7 w-7" />
+          <span className="absolute -top-1 -right-1 flex h-3 w-3 items-center justify-center rounded-full bg-red-500 text-xs text-white"></span>
+        </button>
+
+        {/* User Avatar */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="h-8 w-8 cursor-pointer">
+              <AvatarImage src="/user.jpg" alt="User avatar" />
+              <AvatarFallback></AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56" align="end">
+            {/* <DropdownMenuLabel>
             <div className="flex flex-col space-y-1">
               <p className="text-sm font-medium">ប្រវត្តិរូប</p>
               <p className="text-xs text-muted-foreground">name@example.com</p>
             </div>
           </DropdownMenuLabel> */}
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <User className="mr-2 h-4 w-4" />
-              <span>ប្រវត្តិរូប</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Settings className="mr-2 h-4 w-4" />
-              <span>ដាស់ផ្ទាំងគ្រប់គ្រង</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <div className="flex w-full items-center justify-between">
-                <div className="flex items-center">
-                  <Moon className="mr-2 h-4 w-4" />
-                  <span>មុខងារងងឹត</span>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>ប្រវត្តិរូប</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Settings className="mr-2 h-4 w-4" />
+                <span>ដាស់ផ្ទាំងគ្រប់គ្រង</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <div className="flex w-full items-center justify-between">
+                  <div className="flex items-center">
+                    <Moon className="mr-2 h-4 w-4" />
+                    <span>មុខងារងងឹត</span>
+                  </div>
+                  <Switch />
                 </div>
-                <Switch />
-              </div>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-red-600">
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>ចាកចេញ</span>
             </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="text-red-600">
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>ចាកចេញ</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-   </div>
-      </Navbar>
-    );
-  }
-  
-  export default NavbarLogin;
-  
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </Navbar>
+  );
+}
+
+export default NavbarLogin;
