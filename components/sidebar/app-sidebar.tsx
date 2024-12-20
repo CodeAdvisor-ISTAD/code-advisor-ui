af0eb7d0e9229b439d8bb77b0057b695ecad9345
Yith Sopheaktra <yithsopheaktra18@gmail.com>
2024-12-20 15:46:23 +0700

    route on sidebar

--- components/sidebar/app-sidebar.tsx
+++ components/sidebar/app-sidebar.tsx
@@ -34,13 +34,13 @@ const items = [
   {
     id: 1,
     title: "អត្ថបទ",
-    url: "#",
+    url: "/home",
     icon: Home,
     subItems: [
       {
         id: 1,
         title: "ថ្មីៗ",
-        url: "#",
+        url: "/home",
       },
       {
         id: 2,
@@ -50,14 +50,14 @@ const items = [
       {
         id: 3,
         title: "ស្លាក",
-        url: "#",
+        url: "/content/tags",
       },
     ],
   },
   {
     id: 18,
     title: "សំណួរទូទៅ",
-    url: "/forum",
+    url: "",
     icon: FileQuestion,
     subItems: [
       {
@@ -68,12 +68,12 @@ const items = [
       {
         id: 2,
         title: "ពេញនិយម",
-        url: "/trending",
+        url: "/forum",
       },
       {
         id: 3,
         title: "ស្លាក",
-        url: "/tag",
+        url: "/forum/tags",
       },
     ],
   },
