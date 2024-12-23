8274b6c3796deb194e03883a86a2e037bc0a932e
sk <mrrkhann9u@email.com>
2024-12-23 11:35:06 +0700

    hide scrollbar

--- app/content/layout.tsx
+++ app/content/layout.tsx
@@ -12,9 +12,6 @@ export default function ContentLayout({
     <div
         className={`${roboto.variable} ${koh_Santepheap.variable} h-full relative`}
       >
-        <header className="bg-white fixed top-0 right-0 left-0 z-50">
-          <NavbarComponent />
-        </header>
 
         <SidebarProvider>
           <main className="bg-gray-100 w-full ">{children}</main>
