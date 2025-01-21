45ba2bd7a1ae8319e6d4acaba88f0f40306784f5
vanndeth <artvandeth2001@gmail.com>
2025-01-21 10:07:07 +0700

    change from notification's port to gateway's port

--- app/notification/page.tsx
+++ app/notification/page.tsx
@@ -47,7 +47,7 @@ export default function NotificationsPage() {
     setUserId(currentUserId);
 
     const wsService = new WebSocketService(
-      "http://localhost:8168/ws",
+      "http://127.0.0.1:8168/notifications/ws",
       currentUserId
     );
 
