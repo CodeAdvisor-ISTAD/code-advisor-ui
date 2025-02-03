1ec483b5fe51dc32efd1606010316da3bfc04f3d
Yith Sopheaktra <yithsopheaktra18@gmail.com>
2025-02-03 09:08:46 +0700

    change client in login navbar

--- components/navbar/NavbarComponent.tsx
+++ components/navbar/NavbarComponent.tsx
@@ -103,7 +103,7 @@ export default function NavbarComponent({ onSearch }: NavbarComponentProps) {
           {/* Action Buttons */}
           <div className="flex h-[50px]">
             <Button
-              onClick={() => route.push('/oauth2/authorization/code-advisor-prod')}
+              onClick={() => route.push('/oauth2/authorization/code-advisor')}
               className="m-[8px] text-white bg-primary rounded-[5px] hover:bg-primary-dark transition-colors"
             >
               Login
